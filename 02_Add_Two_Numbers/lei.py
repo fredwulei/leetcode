47f4b3168a531e550132e73e8ba021ecfe1463c3
Fred Wu <fred.wulei@gmail.com>
2015-05-23 23:19:08 -0500

    change format of 02

--- 02_Add_Two_Numbers/lei.py
+++ 02_Add_Two_Numbers/lei.py
@@ -9,18 +9,18 @@ class Solution:
     # @param {ListNode} l2
     # @return {ListNode}
     def addTwoNumbers(self, l1, l2):
-    	l = ListNode(None)
-    	head = l
-    	carry = 0
-    	e1 = l1
-    	e2 = l2
-    	while e1!=None or e2!=None or carry!=0:
-    		v1 = e1.val if e1!=None else 0
-    		v2 = e2.val if e2!=None else 0
-    		d = v1+v2+carry
-    		carry = d/10
-		l.next = ListNode(d%10)
-    		l = l.next
-    		e1 = e1.next if e1!=None else None
-    		e2 = e2.next if e2!=None else None
-    	return head.next
+        l = ListNode(None)
+        head = l
+        carry = 0
+        e1 = l1
+        e2 = l2
+        while e1!=None or e2!=None or carry!=0:
+           v1 = e1.val if e1!=None else 0
+           v2 = e2.val if e2!=None else 0
+           d = v1+v2+carry
+           carry = d/10
+           l.next = ListNode(d%10)
+           l = l.next
+           e1 = e1.next if e1!=None else None
+           e2 = e2.next if e2!=None else None
+        return head.next
